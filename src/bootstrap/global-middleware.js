c3871122495323634c404fef767dc20b8a596538
Md. Harun Or Rashid <horrashid007@gmail.com>
2024-05-22 16:52:33 +0600

    Implemenred the rate limit

--- src/bootstrap/global-middleware.js
+++ src/bootstrap/global-middleware.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import rateLimit from "express-rate-limit";
 
 const corsOptions = {
   origin: process.env.CORS_ORIGIN,
@@ -15,6 +16,14 @@ const globalMiddleware = (app) => {
   if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
   }
+
+  // Limit request with same API
+  const limiter = rateLimit({
+    max: 10,
+    windowMs: 60 * 1000, //Its allowed 10 request from same IP with 1 minute
+    message: "To many requests with this IP, Please try in an hour!",
+  });
+  app.use("/api", limiter);
 
   // Body parser, reading data from body
   app.use(express.json({ limit: "10kb" }));
