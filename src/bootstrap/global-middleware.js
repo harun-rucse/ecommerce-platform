5f1d2e35b590f2b8aaa4862f85d963594d7dbfbe
Md. Harun Or Rashid <horrashid007@gmail.com>
2024-05-22 18:55:37 +0600

    Add security to the backend

--- src/bootstrap/global-middleware.js
+++ src/bootstrap/global-middleware.js
@@ -2,6 +2,11 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import xss from "xss-clean";
+import hpp from "hpp";
+import compression from "compression";
 
 const corsOptions = {
   origin: process.env.CORS_ORIGIN,
@@ -11,6 +16,9 @@ const corsOptions = {
 const globalMiddleware = (app) => {
   // Enable CORS request
   app.use(cors(corsOptions));
+
+  // Set security HTTP headers
+  app.use(helmet());
 
   //Development Logging
   if (process.env.NODE_ENV === "development") {
@@ -28,6 +36,18 @@ const globalMiddleware = (app) => {
   // Body parser, reading data from body
   app.use(express.json({ limit: "10kb" }));
   app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+
+  // Data sanitization against NOSQL query injection
+  app.use(mongoSanitize());
+
+  // Data sanitization against XSS
+  app.use(xss());
+
+  // Prevent parameter polution
+  app.use(hpp());
+
+  // Compression
+  app.use(compression());
 };
 
 export default globalMiddleware;
